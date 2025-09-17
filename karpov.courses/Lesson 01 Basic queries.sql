272396db044cac07d425a02290d235bcd1de0dae
Кирилл <strelkov_ka@mail.ru>
2025-09-17 16:29:42 +0300

    Update Lesson 01 Basic queries.sql
    Lesson 01 Basic queries

--- karpov.courses/Lesson 01 Basic queries.sql
+++ karpov.courses/Lesson 01 Basic queries.sql
@@ -3,5 +3,11 @@
 -- Поля в результирующей таблице: product_id, name, price
 SELECT product_id, name, price
 FROM products
+-- Задание:
+-- Выведите все записи из таблицы products, отсортировав их по наименованиям товаров в алфавитном порядке, т.е. по возрастанию. Для сортировки используйте оператор ORDER BY.
+-- Поля в результирующей таблице: product_id, name, price
+SELECT product_id, name, price
+FROM products
+ORDER BY name ASC
 
 
